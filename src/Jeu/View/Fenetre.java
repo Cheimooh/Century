e6240c636783263f7d71da7f211109a4bd1d5ded
theo.collignon@edu.univ-fcomte.fr <theo.collignon2506@gmail.com>
2018-11-16 13:56:48 +0100

    Affichage du nombre de carte d'un joueur

--- src/Jeu/View/Fenetre.java
+++ src/Jeu/View/Fenetre.java
@@ -55,6 +55,14 @@ public class Fenetre extends Parent {
 
     public void tourSuivant(){
         afficherMainDuJoueur();
+        Color color = Color.LIGHTGREY;
+        graphicsContext.setFill(color);
+        graphicsContext.fillRect(0, 0, 250, height);
+        for (int i = 0; i < century.getNbJoueur(); i++) {
+            int hauteur = (height/century.getNbJoueur())*i;
+            drawLine(0,hauteur-1,250,hauteur-1);
+            afficherJoueur(i,hauteur);
+        }
     }
 
     private void afficherMainDuJoueur() {
@@ -86,6 +94,10 @@ public class Fenetre extends Parent {
     private void afficherJoueur(int numJoueur, int hauteur) {
         String nom = century.getTabJoueur()[numJoueur].getNom();
         graphicsContext.strokeText(nom, 20, hauteur+20);
+        graphicsContext.setFill(Color.LIGHTBLUE);
+        graphicsContext.fillRect(10, hauteur+30, 20, 30);
+        int nbCarte = century.getTabJoueur()[numJoueur].getListeCartes().size();
+        graphicsContext.strokeText("x"+nbCarte, 35, hauteur+55);
     }
 
     private void drawLine(int x1, int y1, int x2, int y2) {
